cb1e314e953e3aba8f90e9fd76a989229872342d
Yuhuan Qiu <yqiu58@gmail.com>
2016-11-12 00:46:06 -0500

    dummy client, server implementations

---
+++ src_exp/client.ml
@@ -0,0 +1,80 @@
+(*
+ * client.ml
+ * Copyright (C) 2016 yqiu <yqiu@f24-suntzu>
+ *
+ * Distributed under terms of the MIT license.
+ *)
+(*
+ * client
+ * client host port establishes a connection on the port port of the machine named
+ * host, sends on the resulting socket the data it reads on its standard input and
+ * writes the data it receives on its standard output.
+ *)
+
+open Sys
+open Unix
+
+(* lib function Misc.retransmit fdin fdout reads data on the descriptor fdin *)
+(* and writes it on fdout *)
+let retransmit fdin fdout =
+	let buffer_size = 4096 in
+	let buffer = Bytes.create buffer_size in
+  (* [ reads fd buff ofs len ] reads len bytes from descriptor fd, stores them *)
+  (* in byte sequence buff, starting at position ofs in buff.
+   * Returns number of bytes actually read *)
+	let rec copy () = match read fdin buffer 0 buffer_size with
+		 | 0 -> ()
+     (* [write fd buff ofs len] writes len bytes to descriptor fd, taking them *)
+     (* from byte sequence buff, starting at position ofs in buff. Returns number *)
+     (* of bytes actually written *)
+		 | n -> ignore (write fdout buffer 0 n); copy ()
+	in copy ()
+
+let server_name = Sys.argv.(1)
+let port_number = int_of_string Sys.argv.(2)
+
+let is_valid_args () =
+  if Array.length Sys.argv < 3 then
+    begin
+      prerr_endline "Usage: client <host> <port>";
+      exit 2
+    end
+
+let get_server_addr server_name =
+  try
+    (gethostbyname server_name).h_addr_list.(0)
+  with Not_found ->
+    prerr_endline (server_name ^ ": Host not found");
+    exit 2
+
+let client () =
+  is_valid_args ();
+  let server_addr = get_server_addr server_name in
+  let sock = socket PF_INET SOCK_STREAM 0 in
+  (* system call connect establishes a connection with a server on a socket. *)
+  (* client side socket here *)
+  connect sock (ADDR_INET(server_addr, port_number));
+  (********************************
+  *  A bit lost here on fork ()  *
+  ********************************)
+  (* val fork : unit -> int *)
+  (* fork a new process. The returned integer is 0 for the child process, the pid *)
+  (* of the child process for the parent process *)
+  match fork () with
+  | 0 ->
+      (* stdin = file descriptor for standard input *)
+      (* child process copies the data from its stdin to the socket *)
+      retransmit stdin sock;
+      (* close the socket for writing only.  *)
+      shutdown sock SHUTDOWN_SEND;
+      exit 0
+  | _ ->
+      (* read from sock and write to stdout *)
+      (* parent process copies data it reads on the socket to its stdout *)
+      retransmit sock stdout;
+      (* closes the read and write sides of the connectoin, and deallocates the *)
+      (* socket *)
+      close stdout;
+      wait ()
+
+let _ = handle_unix_error client ()
---
+++ src_exp/server.ml
@@ -0,0 +1,84 @@
+(*
+ * server.ml
+ * Copyright (C) 2016 yqiu <yqiu@f24-suntzu>
+ *
+ * Distributed under terms of the MIT license.
+ *)
+
+open Lwt
+
+(* make server listen on 127.0.0.1:9000 *)
+let listen_address = Unix.inet_addr_loopback (* or Sys.argv.(1) *)
+let port = 9000 (* or Sys.argv.(2) *)
+let backlog = 10 (* max num of connections? not working *)
+
+(* enable logging up to the INFO level *)
+let () = Lwt_log.add_rule "*" Lwt_log.Info
+
+let handle_message msg =
+    msg
+    (* match msg with *)
+    (* | "read" -> string_of_int !counter *)
+    (* | "inc"  -> counter := !counter + 1; "Counter has been incremented" *)
+    (* | _      -> "Unknown command" *)
+
+let rec handle_connection ic oc () =
+    Lwt_io.read_line_opt ic >>=
+    (fun msg ->
+        match msg with
+        | Some msg ->
+            let reply = handle_message msg in
+            Lwt_io.write_line oc reply >>= handle_connection ic oc
+        | None -> Lwt_log.info "Connection closed" >>= return)
+
+(* [greeting oc] is a message/prompt the server sends to a new connection  *)
+let greeting oc =
+  ignore (Lwt_io.write_line oc "Welcome.")
+
+(*
+ * [accept_connection conn] 'accepts' a connection from
+ * [conn : descriptor * sockaddr] and creates a channel to the file descriptor,
+ * sends a greeting, and calls [handle_connection]
+ *)
+let accept_connection conn =
+    let fd, sckaddr = conn in
+    let open Lwt_unix in
+    let client_id =
+      match sckaddr with
+      | ADDR_INET(inet_addr,num) ->
+          let open Unix in
+          string_of_inet_addr inet_addr
+      | _ -> "unknown" in
+    let ic = Lwt_io.of_fd Lwt_io.Input fd in
+    let oc = Lwt_io.of_fd Lwt_io.Output fd in
+    greeting oc;
+    Lwt.on_failure (handle_connection ic oc ()) (fun e -> Lwt_log.ign_error (Printexc.to_string e));
+    Lwt_log.info ("New connection from " ^ client_id) >>= return
+
+(*
+ * [create_socket () ] creates a socket of type stream in the internet
+ * domain with the default protocol and returns it
+ *)
+let create_socket () =
+    let open Lwt_unix in
+    let sock = socket PF_INET SOCK_STREAM 0 in
+    bind sock @@ ADDR_INET(listen_address, port);
+    listen sock backlog;
+    sock
+
+(*
+ * [create_server] creates a socket with [create_socket] and enters an infinite
+ * loop. At each iteration of the loop, it waits for a connection request with
+ * accept and treats it with [accept_connection].
+ *)
+let create_server () =
+  let server_socket = create_socket () in
+  let rec serve () =
+    let client = Lwt_unix.accept server_socket in
+    client >>= accept_connection >>= serve
+  in serve
+
+(* initialize the server *)
+let () =
+    let serve = create_server () in
+    Lwt_main.run @@ serve ()
